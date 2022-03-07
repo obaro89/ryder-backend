05425cecd3f60a44c9a86706e6be0494c3574452
obaro89 <igbinobaroosaretin@gmail.com>
2022-03-07 22:03:23 +0000

    deployed to heroku

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const connectToDB = require('./config/database');
+const cors = require("cors");
+const connectToDB = require("./config/database");
 
 connectToDB();
 
-app.get('/', (req, res) => {
-	res.send('Backend service is running');
-});
-
 app.use(
-	express.json({
-		extended: false,
-	}),
+  express.json({
+    extended: false,
+  })
 );
 app.use(cors());
-app.use('/api/user', require('./routes/api/user'));
-app.use('/api/rider', require('./routes/api/rider'));
-app.use('/api/booking', require('./routes/api/booking'));
+app.use("/api/user", require("./routes/api/user"));
+app.use("/api/rider", require("./routes/api/rider"));
+app.use("/api/booking", require("./routes/api/booking"));
+
+if (process.env.NODE_ENV === "production") {
+  app.get("/", (req, res) => {
+    res.send("Backend service is running");
+  });
+}
 
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
-	console.log('server started at port ' + PORT);
+  console.log("server started at port " + PORT);
 });
